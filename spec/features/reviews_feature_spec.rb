fecb52d270004c429307fc062275beb9b7190108
Archie Fielding <archiefielding@gmail.com>
2015-10-13 12:49:09 +0100

    Made sure reviews are deleted with restaurants

--- spec/features/reviews_feature_spec.rb
+++ spec/features/reviews_feature_spec.rb
@@ -13,4 +13,16 @@ feature 'reviewing' do
      expect(current_path).to eq '/restaurants'
      expect(page).to have_content('so so')
   end
+
+  scenario 'if a restaurant is deleted, its reviews should also be deleted' do
+    visit '/restaurants'
+    click_link 'Review KFC'
+    fill_in "Thoughts", with: "so so"
+    select '3', from: 'Rating'
+    click_button 'Leave Review'
+
+    expect(current_path).to eq '/restaurants'
+    expect { click_link 'Delete KFC' }.to change(Review, :count).by(-1)
+  end
+
 end
